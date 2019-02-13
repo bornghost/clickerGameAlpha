05178213a893dd5d13626e281841b18cb4023d71
bornghost <47609111+bornghost@users.noreply.github.com>
2019-02-13 14:20:10 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var cookies = 0;
 var helpers = 0;
 var cookieMakers = 0;
 var cps = helpers + (cookieMakers*5);
+var helperCost = 0;
+var nextHelperCost = 0;
+var cookieMakerCost = 0;
+var nextCookieMakerCost = 0;
 
 function cookieClick(number){
 	cookies = cookies + number;
@@ -9,30 +13,33 @@ function cookieClick(number){
 };
 
 function buyHelper(){
-	var helperCost = Math.floor(10 * Math.pow(1.1,helpers));		//works out the cost of this cursor
+	helperCost = Math.floor(10 * Math.pow(1.1,helpers));
 	if(cookies >= helperCost){										//checks player can afford
 		helpers = helpers + 1;										//increases number of cursors
 		cookies = cookies - helperCost;								//removes the cookies spent
 		document.getElementById('helpers').innerHTML = helpers;		//updates cursor count
 		document.getElementById('cookies').innerHTML = cookies;		//updates cookie count
 	};
-	var nextHelperCost = Math.floor(10 * Math.pow(1.1,helpers));			//find cost of next cursor
+	nextHelperCost = Math.floor(10 * Math.pow(1.1,helpers));			//find cost of next cursor
 	document.getElementById('helperCost').innerHTML = nextHelperCost;		//updates next cursor cost
 };
 
 function buyCookieMaker(){
-	var cookieMakerCost = Math.floor(5 * Math.pow(2.1,cookieMakers));		//works out the cost of this cursor
+	cookieMakerCost = Math.floor(5 * Math.pow(2.1,cookieMakers));		//works out the cost of this cursor
 	if(helpers >= cookieMakerCost){										//checks player can afford
 		cookieMakers = cookieMakers + 1;										//increases number of cursors
 		helpers = helpers - cookieMakerCost;								//removes the cookies spent
 		document.getElementById('cookieMakers').innerHTML = cookieMakers;		//updates cursor count
 		document.getElementById('helpers').innerHTML = helpers;		//updates cookie count
 	};
-	var nextCookieMakerCost = Math.floor(5 * Math.pow(2.1,cookieMakers));			//find cost of next cursor
+	helperCost = Math.floor(10 * Math.pow(1.1,helpers));
+	nextCookieMakerCost = Math.floor(12 * Math.pow(2.1,cookieMakers));			//find cost of next cursor
 	document.getElementById('cookieMakerCost').innerHTML = nextCookieMakerCost;		//updates next cursor cost
+	nextHelperCost = Math.floor(10 * Math.pow(1.1,helpers));
+	document.getElementById('helperCost').innerHTML = nextHelperCost;
 };
 
 window.setInterval(function(){
 	cookieClick(helpers);
-	cookieClick(cookieMakers*5);
+	cookieClick(cookieMakers*12);
 }, 1000);
