5de80198b52c54788f606fa8f531797428ffbb30
bornghost <47609111+bornghost@users.noreply.github.com>
2020-07-22 11:20:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,65 @@
 var cookies = 0;
-var helpers = 0;
-var cookieMakers = 0;
-var cps = helpers + (cookieMakers*5);
-var helperCost = 0;
-var nextHelperCost = 0;
-var cookieMakerCost = 0;
-var nextCookieMakerCost = 0;
+var cursors = 0;
+var ovens = 0;
+var bakeries = 0;
+var factories = 0;
+var cps = 0;
 
 function cookieClick(number){
 	cookies = cookies + number;
+	cps = number;
 	document.getElementById("cookies").innerHTML = cookies;
+	document.getElementById("cps").innerHTML = cps;
 };
 
-function buyHelper(){
-	helperCost = Math.floor(10 * Math.pow(1.1,helpers));
-	if(cookies >= helperCost){										//checks player can afford
-		helpers = helpers + 1;										//increases number of cursors
-		cookies = cookies - helperCost;								//removes the cookies spent
-		document.getElementById('helpers').innerHTML = helpers;		//updates cursor count
-		document.getElementById('cookies').innerHTML = cookies;		//updates cookie count
+function buyCursor(){
+	var cursorCost = Math.floor(10 * Math.pow(1.2,cursors));
+	if(cookies >= cursorCost){
+		cursors = cursors + 1;
+		cookies = cookies - cursorCost;
+		document.getElementById('cursors').innerHTML = cursors;
+		document.getElementById('cookies').innerHTML = cookies;
 	};
-	nextHelperCost = Math.floor(10 * Math.pow(1.1,helpers));			//find cost of next cursor
-	document.getElementById('helperCost').innerHTML = nextHelperCost;		//updates next cursor cost
+	var nextCursorCost = Math.floor(10 * Math.pow(1.2,cursors));
+	document.getElementById('cursorCost').innerHTML = nextCursorCost;
 };
 
-function buyCookieMaker(){
-	cookieMakerCost = Math.floor(5 * Math.pow(2.1,cookieMakers));		//works out the cost of this cursor
-	if(helpers >= cookieMakerCost){										//checks player can afford
-		cookieMakers = cookieMakers + 1;										//increases number of cursors
-		helpers = helpers - cookieMakerCost;								//removes the cookies spent
-		document.getElementById('cookieMakers').innerHTML = cookieMakers;		//updates cursor count
-		document.getElementById('helpers').innerHTML = helpers;		//updates cookie count
+function buyOven(){
+	var ovenCost = Math.floor(250 * Math.pow((1.2+(.05*ovens)),ovens));
+	if(cookies >= ovenCost){
+		ovens = ovens + 1;
+		cookies = cookies - ovenCost;
+		document.getElementById('ovens').innerHTML = ovens;
+		document.getElementById('cookies').innerHTML = cookies;
 	};
-	helperCost = Math.floor(10 * Math.pow(1.1,helpers));
-	nextCookieMakerCost = Math.floor(12 * Math.pow(2.1,cookieMakers));			//find cost of next cursor
-	document.getElementById('cookieMakerCost').innerHTML = nextCookieMakerCost;		//updates next cursor cost
-	nextHelperCost = Math.floor(10 * Math.pow(1.1,helpers));
-	document.getElementById('helperCost').innerHTML = nextHelperCost;
+	var nextOvenCost = Math.floor(250 * Math.pow((1.2+(.05*ovens)),ovens));
+	document.getElementById('ovenCost').innerHTML = nextOvenCost;
+};
+
+function buyBakery(){
+	var bakeryCost = Math.floor(2000 * Math.pow((1.2+(.075*bakeries)),bakeries));
+	if(cookies >= bakeryCost){
+		bakeries = bakeries + 1;
+		cookies = cookies - bakeryCost;
+		document.getElementById('bakeries').innerHTML = bakeries;
+		document.getElementById('cookies').innerHTML = cookies;
+	};
+	var nextBakeryCost = Math.floor(2000 * Math.pow((1.2+(.075*bakeries)),bakeries));
+	document.getElementById('bakeryCost').innerHTML = nextBakeryCost;
+};
+
+function buyFactory(){
+	var factoryCost = Math.floor(10000 * Math.pow((1.2+(.09*factories)),factories));
+	if(cookies >= factoryCost){
+		factories = factories + 1;
+		cookies = cookies - factoryCost;
+		document.getElementById('factories').innerHTML = factories;
+		document.getElementById('cookies').innerHTML = cookies;
+	};
+	var nextFactoryCost = Math.floor(2000 * Math.pow((1.2+(.09*factories)),factories));
+	document.getElementById('factoryCost').innerHTML = nextFactoryCost;
 };
 
 window.setInterval(function(){
-	cookieClick(helpers);
-	cookieClick(cookieMakers*12);
+	cookieClick(cursors + (ovens*6) + (bakeries*24) + (factories*96));
 }, 1000);
